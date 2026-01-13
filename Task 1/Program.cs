224b073151f1ad93dc49546befbf5103b790a147
jabesalex17-byte <jabesalex17@gmail.com>
2026-01-13 17:38:53 -0400

    The fist homework

---
+++ Task 1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Jabes Esteves 2025-0905
+
+int age = 5;
+bool istrue = false;
+string name = "Jabes";
+
+Console.WriteLine($"My name  is {name}, my age is {age}, this is true {istrue}");
+
+const int healt = 10;
+
+Console.WriteLine(healt);
+
+//healt = 23;
+
+Console.WriteLine(healt);
+
+int num = 2;
+
+Console.WriteLine(num);
+
+num++;
+num--;
+num += 5;
+
+Console.WriteLine(num + 5);
+
+float value = 10152466.25f;
+
+byte byte1 = 5;
+
+byte sum = (byte)(byte1 + value);
+
+//this is a comment
+
+/*
+ this is a comment to
+ */
+
+DateTime time = DateTime.Now;
+Console.WriteLine(time);
