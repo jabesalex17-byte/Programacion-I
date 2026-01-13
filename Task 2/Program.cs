2698cd50b813ce53b1014e5d01a4b6eefb1b26d4
jabesalex17-byte <jabesalex17@gmail.com>
2026-01-13 18:15:35 -0400

    Add files via upload

---
+++ Task 2/Program.cs
@@ -0,0 +1,36 @@
+﻿List<string> answers = ["yes","si","y","ye"];
+while (true)
+{
+    Console.Clear();
+    try
+    {
+
+        Console.WriteLine("insert a number");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(VerifyNumber(num));
+      
+        Console.WriteLine("\nDo you wanna exit?");
+        string answer = Console.ReadLine()!;
+        if (answers.Contains(answer.ToLower().Trim())){
+            break;
+        }
+
+    }
+    catch
+    {
+        continue;
+    }
+    
+    
+}
+
+static string VerifyNumber(int x)
+{
+    if(x == 0)
+    {
+        return "The number is  invalid";
+    }
+    var message = x % 2 == 0 ? "The number is par" : "The number is impar";
+    return message;
+}
