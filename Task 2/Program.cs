41907d8afa15dd0c2b7e920f834e3d8bcaa6f60a
Jabes Estevez <jabesalex17@gmail.com>
2026-02-05 19:31:29 -0400

    Update Program.cs

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,39 +1,26 @@
 //Jabes Estevez 2025-0905
-List<string> answers = ["yes","si","y","ye"];
+List<string> answers = ["yes","si","y","ye","ss"];
 while (true)
 {
     Console.Clear();
-    try
-    {
-
         Console.WriteLine("insert a number");
-        int num = Convert.ToInt32(Console.ReadLine());
-
+        if(!int.TryParse(Console.ReadLine(), out int num))
+        {
+            continue;
+        }
         Console.WriteLine(VerifyNumber(num));
-      
         Console.WriteLine("\nDo you wanna exit?");
         string answer = Console.ReadLine()!;
         if (answers.Contains(answer.ToLower().Trim())){
             break;
         }
-
-    }
-    catch
-    {
-        continue;
-    }
-    
-    
 }
-
 static string VerifyNumber(int x)
 {
     if(x == 0)
     {
         return "The number is  invalid";
     }
-    var message = x % 2 == 0 ? "The number is pair" : "The number is odd";
+    var message = x % 2 == 0 ? "The number is par" : "The number is impar";
     return message;
-
 }
-
